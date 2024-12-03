34b54565c4d9476e7c4fa1526d4ccc34ae338e7a
wanlizhu <wanlizhu@hotmail.com>
2024-12-02 23:19:55 -0800

    s

--- startup.sh
+++ startup.sh
@@ -5,11 +5,17 @@ fi
 
 if [[ ! -z $(which ifconfig) ]]; then
     ip=$(ifconfig | grep "172.16" | awk '{print $2}')
-    if [[ -z $(grep "$ip $HOSTNAME" $HOME/wanliz_linux_workbench/hosts) ]]; then
-        if [[ ! -z $(grep " $HOSTNAME" $HOME/wanliz_linux_workbench/hosts) ]]; then
-            sed -i "/ $HOSTNAME$/d" $HOME/wanliz_linux_workbench/hosts
+    case $HOSTNAME in
+        "scc-03-3062-dev")  host=dev ;;
+        "scc-03-3062-test") host=test ;;
+        "scc-03-3062-wfh")  host=wfh ;;
+        *) host=$HOSTNAME ;;
+    esac
+    if [[ -z $(grep "$ip $host" $HOME/wanliz_linux_workbench/hosts) ]]; then
+        if [[ ! -z $(grep " $host" $HOME/wanliz_linux_workbench/hosts) ]]; then
+            sed -i "/ $host$/d" $HOME/wanliz_linux_workbench/hosts
         fi
-        echo "$ip $HOSTNAME" >> $HOME/wanliz_linux_workbench/hosts
+        echo "$ip $host" >> $HOME/wanliz_linux_workbench/hosts
         cd $HOME/wanliz_linux_workbench
         git add .
         git commit -m "update hosts"
