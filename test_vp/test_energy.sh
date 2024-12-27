22769957271b265ff097e7c761f64d73082fbb39
Wanli Zhu <zhu.wanli@icloud.com>
2024-12-27 15:24:38 -0800

    uu

---
+++ test_vp/test_energy.sh
@@ -0,0 +1,11 @@
+if [[ ! -f ./viewperf/bin/viewperf ]]; then
+    cd ~/viewperf2020 
+fi
+
+echo "- 1920x1080"
+echo "- 3840x2160"
+read -e -i "1920x1080" -p "Resolution: " size
+
+mkdir -p results/energy-03/ &&
+./viewperf/bin/viewperf viewsets/energy/config/energy.xml -resolution $size && 
+cat results/energy-03/results.xml
--- test_vp/test_snx.sh
+++ test_vp/test_snx.sh
@@ -7,5 +7,5 @@ echo "- 3840x2160"
 read -e -i "1920x1080" -p "Resolution: " size
 
 mkdir -p results/snx-04/ &&
-./viewperf/bin/viewperf viewsets/snx/config/snx.xml -resolution $size && 
-cat results/snx-04/results.xml
+./viewperf/bin/viewperf viewsets/snx/config/snx.xml -resolution $size &&
+grep '<Test Index=' results/snx-04/results.xml | awk -F '"' '{print $10}' 
