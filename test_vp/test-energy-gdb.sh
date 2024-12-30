dd81cf3509fd2545e77e58f2f463acdea206b71e
wanlizhu <wanlizhu@hotmail.com>
2024-12-31 06:13:20 +0900

    uu

--- test_vp/test-energy-gdb.sh
+++ test_vp/test-energy-gdb.sh
@@ -7,5 +7,5 @@ echo "- 3840x2160"
 read -e -i "1920x1080" -p "Resolution: " size
 
 mkdir -p results/energy-03/ &&
-gdb --args ./viewperf/bin/viewperf viewsets/energy/config/energy.xml -resolution $size && 
+gdb -ex "run" --args ./viewperf/bin/viewperf viewsets/energy/config/energy.xml -resolution $size && 
 cat results/energy-03/results.xml
