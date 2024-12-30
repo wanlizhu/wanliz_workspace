b27bd443afc6ab8cbb13d4cd6eba112389fa5460
wanlizhu <wanlizhu@hotmail.com>
2024-12-31 07:39:27 +0900

    uu

--- test-env.sh
+++ test-env.sh
@@ -731,7 +731,7 @@ function pull-dvs-source {
 }
 
 function gdb-attach-xorg {
-    sudo gdb $(which Xorg) $(pgrep Xorg)
+    sudo gdb -ex "handle SIGPIPE nostop noprint pass" -ex "cont" $(which Xorg) $(pgrep Xorg)
 }
 
 ###########################################################
