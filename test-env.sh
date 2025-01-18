9a338e0d17718dea92aab4a4165f8f60d1baf56a
wanlizhu <wanlizhu@hotmail.com>
2025-01-18 01:02:21 -0800

    uu

--- test-env.sh
+++ test-env.sh
@@ -1182,9 +1182,23 @@ if [[ $ans == yes ]]; then
     if [[ -z $(which openconnect) ]]; then
         sudo apt install -y openconnect
     fi
-    read -e -i "firefox" -p "Complete authentication in browser: " browser
-    read -e -i "no" -p "Run in background? (yes/no): " runinbg
-    eval $(openconnect --useragent="AnyConnect-compatible OpenConnect VPN Agent" --external-browser $(which $browser) --authenticate ngvpn02.vpn.nvidia.com/SAML-EXT)
+
+    if [[ -z $(openconnect --version | head -1 | grep "v9") ]]; then
+        pushd ~/Downloads
+        if [[ ! -z $(lsb_release -r | grep "22") ]]; then
+            wget --no-check-certificate https://download.opensuse.org/repositories/home:/bluca:/openconnect/Ubuntu_22.04/amd64/openconnect_9.12+201+gf17fe20-0+283.1_amd64.deb
+            sudo dpkg -i openconnect_9.12+201+gf17fe20-0+283.1_amd64.deb
+        elif [[ ! -z $(lsb_release -r | grep "24") ]]; then
+            wget --no-check-certificate https://download.opensuse.org/repositories/home:/bluca:/openconnect/Ubuntu_24.04/amd64/openconnect_9.12+201+gf17fe20-0+283.1_amd64.deb
+            sudo dpkg -i openconnect_9.12+201+gf17fe20-0+283.1_amd64.deb
+        else
+            echo "Download openconnect manually: https://software.opensuse.org//download.html?project=home%3Abluca%3Aopenconnect&package=openconnect"
+            exit -1
+        fi
+        popd 
+    fi
+
+    eval $(openconnect --useragent="AnyConnect-compatible OpenConnect VPN Agent" --external-browser firefox --authenticate ngvpn02.vpn.nvidia.com/SAML-EXT)
     [ -n ["$COOKIE"] ] && echo -n "$COOKIE" | sudo openconnect --cookie-on-stdin $CONNECT_URL --servercert $FINGERPRINT --resolve $RESOLVE 
 fi' > /tmp/vpn-with-sso.sh
             sudo mv /tmp/vpn-with-sso.sh /usr/local/bin/vpn-with-sso.sh
